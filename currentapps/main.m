c87337d91b62710e8249cd0570318d86e77ceac6
Mikhail Gusarov <dottedmag@dottedmag.net>
2022-01-07 20:30:57 +0100

    Initial Commit

---
+++ currentapps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  currentapps
+//
+//  Created by Mikhail Gusarov on 07.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
